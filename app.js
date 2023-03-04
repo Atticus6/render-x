39ae34a1a148d93dc553220f603d55a0c6fc51bb
王亮涛 <94210351+Atticus6@users.noreply.github.com>
2023-03-04 10:14:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/start", (req, res) => {
 });
 
 app.get("/nezha", (req, res) => {
-  let cmdStr = "/bin/bash nezha.sh " + process.env.URL +"  " + process.env.NE_ZHA_PORT +" " +process.env.KEY + "  >/dev/null 2>&1 &" ;
+  let cmdStr = "/bin/bash nezha.sh " + process.env.URL +"  " + process.env.NEZHA_PORT +" " +process.env.KEY + "  >/dev/null 2>&1 &" ;
 
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
@@ -121,7 +121,7 @@ function startWeb() {
 }
 
 function startNezha() {
-  let startNezhaCMD = "/bin/bash nezha.sh " + process.env.URL +"  " + process.env.PORT +" " +process.env.KEY + "  >/dev/null 2>&1 &" ;
+  let startNezhaCMD = "/bin/bash nezha.sh " + process.env.URL +"  " + process.env.NEZHA_PORT +" " +process.env.KEY + "  >/dev/null 2>&1 &" ;
   exec(startNezhaCMD, function (err, stdout, stderr) {
     if (err) {
       console.log("启动哪吒-失败:" + err);
